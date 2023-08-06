b4c987b73acfb9045655a5ad65e1fe3314ab396c
Noel Lim <noellimx@gmail.com>
2023-08-07 01:20:19 +0800

    add ingress 0.1.4-20230805-019

--- getgke.sh
+++ getgke.sh
@@ -10,3 +10,5 @@ gcloud iam service-accounts keys create key.json --iam-account=$SA_EMAIL
 kubectl exec nginx-app-6b6977dd9b-d9m2c -- printenv
 kubectl exec --namespace=forager-helm nginx-app-6b6977dd9b-d9m2c -- printenv
 kubectl exec --namespace=forager-helm nginx-app-98f6b9f4d-4wm78 -- printenv
+
+kubectl exec --namespace=forager-helm -it nginx-app-7bcbbc5f5f-8c5wf -- env
