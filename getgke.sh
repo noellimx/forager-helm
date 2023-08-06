ada67cb01c71289a248b66733bfd8b733b2a2491
Noel Lim <noellimx@gmail.com>
2023-08-06 17:20:54 +0800

    0.1.4-20230805-018 rollback custom config

--- getgke.sh
+++ getgke.sh
@@ -4,3 +4,9 @@
 SA_EMAIL=672059361733-compute@developer.gserviceaccount.com
 
 gcloud iam service-accounts keys create key.json --iam-account=$SA_EMAIL
+
+
+
+kubectl exec nginx-app-6b6977dd9b-d9m2c -- printenv
+kubectl exec --namespace=forager-helm nginx-app-6b6977dd9b-d9m2c -- printenv
+kubectl exec --namespace=forager-helm nginx-app-98f6b9f4d-4wm78 -- printenv
